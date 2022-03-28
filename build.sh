c3c4dfda021c413d724dc2c9f3ac9fafca92863f
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2022-03-27 18:33:52 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ set -x
 set -e
 set -o pipefail
 
-cd openssl_src && CC="$FUZZ_CC $CFLAGS" ./config && make clean && make
+cd openssl_src && CC="clang $CFLAGS" ./config && make clean && make
 cd ..
-$FUZZ_CXX $CXXFLAGS ./target.cc -DCERT_PATH=\"$PWD/runtime\"  openssl_src/libssl.a openssl_src/libcrypto.a $FUZZ_ENGINE -I openssl_src/include -o ./target
+$clang++ $CXXFLAGS ./target.cc -DCERT_PATH=\"$PWD/runtime\"  openssl_src/libssl.a openssl_src/libcrypto.a -fsanitize=fuzzer -I openssl_src/include -o ./target
