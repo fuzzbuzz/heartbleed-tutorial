c42a9d0c7fab54fcc593d023d74cd022369ba5af
Everest Stefan Munro-Zeisberger <evmunro1@gmail.com>
2022-03-27 19:22:06 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ set -x
 set -e
 set -o pipefail
 
+mkdir /testdata && cp runtime/* testdata
 cd openssl_src && CC="clang $CFLAGS" ./config && make clean && make
 cd ..
-clang++ $CXXFLAGS ./target.cc -DCERT_PATH=\"$PWD/runtime\"  openssl_src/libssl.a openssl_src/libcrypto.a -fsanitize=fuzzer -I openssl_src/include -o ./target
+clang++ $CXXFLAGS ./target.cc openssl_src/libssl.a openssl_src/libcrypto.a -fsanitize=fuzzer -I openssl_src/include -o ./target
